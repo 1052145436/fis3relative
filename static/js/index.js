fefaaccf744c4cbd3a36e87e7f102be9ce9e5005
lisx@healthmall.cn <lisx@healthmall.cn>
2017-03-22 11:13:26 +0800

    fis3相对路径搞法

---
+++ static/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/3/22.
+ */
+var indexObj = {
+    init:function(){
+        require(['./jquery'],function($){
+            alert('hello,less!!!');
+        })
+    }
+}
+
+exports.indexObj = indexObj;
